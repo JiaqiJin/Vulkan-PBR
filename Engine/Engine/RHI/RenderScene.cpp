6b0ebc2dffa37102c7b2e7bf62e990a3cb3cb91e
kawakawaDe <fdsam@hotmail.com>
2021-06-11 17:36:28 +0100

    adding scene review

--- Engine/Engine/RHI/RenderScene.cpp
+++ Engine/Engine/RHI/RenderScene.cpp
@@ -1,1 +1,36 @@
 #include "RenderScene.h"
+
+namespace RHI
+{
+	void RenderScene::init(
+		const std::string& vertexShaderFile,
+		const std::string& fragmentShaderFile,
+		const std::string& albedoFile,
+		const std::string& normalFile,
+		const std::string& aoFile,
+		const std::string& shadingFile,
+		const std::string& emissionFile,
+		const std::string& modelFile)
+	{
+		vertexShader.compileFromFile(vertexShaderFile, VulkanShaderKind::Vertex);
+		fragmentShader.compileFromFile(fragmentShaderFile, VulkanShaderKind::Fragment);
+		mesh.loadFromFile(modelFile);
+		albedoTexture.loadFromFile(albedoFile);
+		normalTexture.loadFromFile(normalFile);
+		aoTexture.loadFromFile(aoFile);
+		shadingTexture.loadFromFile(shadingFile);
+		emissionTexture.loadFromFile(emissionFile);
+	}
+
+	void RenderScene::shutdown()
+	{
+		albedoTexture.clearGPUData();
+		normalTexture.clearGPUData();
+		aoTexture.clearGPUData();
+		shadingTexture.clearGPUData();
+		emissionTexture.clearGPUData();
+		mesh.clearGPUData();
+		vertexShader.clear();
+		fragmentShader.clear();
+	}
+}
