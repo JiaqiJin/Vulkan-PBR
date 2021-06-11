c1dee2d0311fd5c7a523907d187c48d27b201af4
kawakawaDe <fdsam@hotmail.com>
2021-06-11 17:30:52 +0100

    mesh reviews

--- Engine/Engine/RHI/VulkanMesh.h
+++ Engine/Engine/RHI/VulkanMesh.h
@@ -8,3 +8,51 @@
 #include <string>
 
 #include "VulkanRendererContext.h"
+
+namespace RHI
+{
+	class VulkanMesh
+	{
+	public:
+		VulkanMesh(const VulkanRendererContext& context)
+			: context(context) { }
+
+		~VulkanMesh();
+
+		// How passing model data format to vertex shader
+		static VkVertexInputBindingDescription getVertexInputBindingDescription();
+		static std::array<VkVertexInputAttributeDescription, 6> getAttributeDescriptions();
+
+		bool loadFromFile(const std::string& filename);
+
+		void uploadToGPU();
+		void clearGPUData();
+		void clearCPUData();
+
+	private:
+		void createVertexBuffer();
+		void createIndexBuffer();
+
+	private:
+		VulkanRendererContext context;
+
+		struct Vertex
+		{
+			glm::vec3 position;
+			glm::vec3 tangent;
+			glm::vec3 binormal;
+			glm::vec3 normal;
+			glm::vec3 color;
+			glm::vec2 uv;
+		};
+
+		std::vector<Vertex> vertices;
+		std::vector<uint32_t> indices;
+
+		VkBuffer vertexBuffer{ VK_NULL_HANDLE };
+		VkDeviceMemory vertexBufferMemory{ VK_NULL_HANDLE };
+
+		VkBuffer indexBuffer{ VK_NULL_HANDLE };
+		VkDeviceMemory indexBufferMemory{ VK_NULL_HANDLE };
+	};
+}
