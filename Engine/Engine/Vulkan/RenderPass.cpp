ec1919c4171d82eb6e2861f0615a991872186201
kawakawaDe <fdsam@hotmail.com>
2021-08-18 18:15:38 +0100

    render pass

--- Engine/Engine/Vulkan/RenderPass.cpp
+++ Engine/Engine/Vulkan/RenderPass.cpp
@@ -1,1 +1,19 @@
 #include "RenderPass.h"
+#include "Device.h"
+#include "../Common/Logger.h"
+
+namespace Vulkan
+{
+	RenderPass::RenderPass(const std::shared_ptr<Device>& device, const VkRenderPassCreateInfo& create_info)
+		: m_device(device)
+	{
+		if (vkCreateRenderPass(device->GetHandle(), &create_info, nullptr, &m_render_pass) != VK_SUCCESS)
+			K_ERROR("Failed to create RenderPass");
+	}
+
+	RenderPass::~RenderPass()
+	{
+		if (m_render_pass)
+			vkDestroyRenderPass(m_device->GetHandle(), m_render_pass, nullptr);
+	}
+}
--- Engine/Engine/Vulkan/RenderPass.h
+++ Engine/Engine/Vulkan/RenderPass.h
@@ -8,5 +8,16 @@
 
 namespace Vulkan
 {
+	class RenderPass : public DeviceObjectBase
+	{
+	public:
+		RenderPass(const std::shared_ptr<Device>& device,const VkRenderPassCreateInfo& create_info);
+		~RenderPass();
+		VkRenderPass GetHandle() const { return m_render_pass; }
+		const std::shared_ptr<Device>& GetDevicePtr() const override { return m_device; }
 
+	private:
+		std::shared_ptr<Device> m_device;
+		VkRenderPass m_render_pass{ VK_NULL_HANDLE };
+	};
 }
