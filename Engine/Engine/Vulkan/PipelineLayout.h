e7642fd56081eacfa2d5cb457cd035f8e20f444c
kawakawaDe <fdsam@hotmail.com>
2021-08-26 21:54:01 +0100

    pipeline layout

--- Engine/Engine/Vulkan/PipelineLayout.h
+++ Engine/Engine/Vulkan/PipelineLayout.h
@@ -8,5 +8,25 @@
 
 namespace Vulkan
 {
+	class DescriptorSetLayout;
 
+	class PipelineLayout : public DeviceObjectBase
+	{
+	public:
+		PipelineLayout(const std::shared_ptr<Device>& device,
+			const std::vector<std::shared_ptr<DescriptorSetLayout>>& descriptor_layouts,
+			const std::vector<VkPushConstantRange>& push_constant_ranges);
+		~PipelineLayout();
+
+		VkPipelineLayout GetHandle() const { return m_pipeline_layout; }
+
+		const std::shared_ptr<Device>& GetDevicePtr() const override { return m_device; }
+		const std::vector<std::shared_ptr<DescriptorSetLayout>>& GetDescriptorSetPtrs() const { return m_descriptor_layout; }
+
+	private:
+		std::shared_ptr<Device> m_device;
+		std::vector<std::shared_ptr<DescriptorSetLayout>> m_descriptor_layout;
+
+		VkPipelineLayout m_pipeline_layout{ VK_NULL_HANDLE };
+	};
 }
