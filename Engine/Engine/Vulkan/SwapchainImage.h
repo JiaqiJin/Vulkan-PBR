f446b2ca1079d350fe2912af70965787b1d639c0
kawakawaDe <fdsam@hotmail.com>
2021-08-26 21:32:56 +0100

    swap chain image

--- Engine/Engine/Vulkan/SwapchainImage.h
+++ Engine/Engine/Vulkan/SwapchainImage.h
@@ -12,8 +12,11 @@ namespace Vulkan
 	class SwapchainImage : public ImageBase
 	{
 	public:
+		static std::vector<std::shared_ptr<SwapchainImage>> Create(const std::shared_ptr<Swapchain>& swapchain);
 
+		const std::shared_ptr<Device>& GetDevicePtr() const override { return m_swapchain->GetDevicePtr(); };
+		const std::shared_ptr<Swapchain>& GetSwapchain() const { return m_swapchain; }
 	private:
-
+		std::shared_ptr<Swapchain> m_swapchain;
 	};
 }
